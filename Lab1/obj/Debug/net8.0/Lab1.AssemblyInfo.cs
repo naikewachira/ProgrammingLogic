fa5ddb7d17f05fddbb1b6d1f43ab0e60a1e4b35b
naikewachira <nwachiraidk@gmail.com>
2025-03-28 19:22:44 +0000

    idk

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a24c3bb860ddbe6eeaf789dbfd9557c916c18c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9a24e7362fe7cecd3b04012d971f78aa1f1ca45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
